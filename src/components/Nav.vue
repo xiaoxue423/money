46357e01006834bd65227a21c22e8d9280ec794f
xiaoxue0423 <xiaoxuexue19980423@163.com>
2021-01-19 18:28:46 +0800

    添加Nav 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
-      <Icon name="label"/>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money"/>
+     记账
+    </router-link>
+
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="labels"/>
       标签
     </router-link>
-    |
-    <router-link to="/labels">
-      <Icon name="money"/>
-      记账
-    </router-link>
-    |
-    <router-link to="/statistics">
+
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -25,8 +25,26 @@ name: "Nav"
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid blue;
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  flex-direction: row;
+  font-size: 12px;
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
+  > .item.selected{
+    color: red;
+  }
 }
 
 </style>
