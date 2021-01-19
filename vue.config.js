3e4c08e691ac2e298bd64ca7e1b2d8f15e1ce48b
xiaoxue0423 <xiaoxuexue19980423@163.com>
2021-01-19 18:23:01 +0800

    创建<Icon />

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+
+    config.module
+        .rule('svg-sprite')
+        .test(/\.svg$/)
+        .include.add(dir).end() // 包含 icons 目录
+        .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
+  }
 }
