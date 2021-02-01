04cafecad61bb8870f8a0f7e2b85b391b19e4760
xiaoxue0423 <xiaoxuexue19980423@163.com>
2021-02-01 22:02:42 +0800

    first

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,39 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Money from "@/views/Money.vue";
-import Labels from "@/views/Labels.vue";
-import Statistics from "@/views/Statistics.vue";
-import NotFound from "@/views/NotFound.vue";
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    redirect: '/money',
-    component: Money
-  },
-  {
-    path:'/money',
-    component: Money
-  },
-  {
-    path:'/labels',
-    component: Labels
-  },
-  {
-    path:'/statistics',
-    component: Statistics
-  },
-  {
-    path: '*',
-    component: NotFound
-  }
-]
+    {
+        path: '/',
+        // 默认路径调转到money
+        redirect: '/money',
+        component: Money
+    },
+    {
+        path: '/money',
+        component: Money
+    },
+    {
+        path: '/labels',
+        component: Labels
+    },
+    {
+        path: '/statistics',
+        component: Statistics
+    },
+    {
+        path: '*',
+        component: NotFound
+    }
+];
 
 const router = new VueRouter({
-  routes
-})
+    routes
+});
 
-export default router
+export default router;
